d5df777213bb35b0e009c250227616a1934fd9b1
Your Name <you@example.com>
2022-04-09 14:00:55 +1200

    added routerLInk

--- src/Viewport/Header.jsx
+++ src/Viewport/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import FingerprintIcon from '@mui/icons-material/Fingerprint';
+import PersonPinCircleRoundedIcon from '@mui/icons-material/PersonPinCircleRounded';
 import AppBar from '@mui/material/AppBar';
 import Link from '@mui/material/Link';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom'
 
 export const Header = () => {
     return (
         <AppBar elevation={1} color="secondary" position="relative" sx={{ padding: 1, }}>
             <Toolbar variant='dense' sx={{ display: 'flex', alignItems: 'center', }}>
-                <Link underline="none" sx={{ display: 'flex', '& svg': { fontSize: '2rem', mr: 1 }, alignItems: 'center', color: 'info.main' }} href="https://credibil.netlify.app/">
-                    <FingerprintIcon alt="" color="info" />
+                <Link underline="none" sx={{ display: 'flex', '& svg': { fontSize: '2rem', mr: 1 }, alignItems: 'center', color: 'info.main' }} component={RouterLink} to="/">
+                    <PersonPinCircleRoundedIcon alt="" color="info" />
                     <Typography variant="h4">  {import.meta.env.VITE_APP_NAME}</Typography>
                 </Link>
             </Toolbar>
